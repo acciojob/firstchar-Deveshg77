1516d1fcedf65396efa33fdf9a2a83511a956ae4
Devesh Sudhir Gunde <deveshgunde77@gmail.com>
2024-07-08 11:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	var trimmedText = text.trim();
+
+  // Check if the trimmed string is empty
+  if (trimmedText.length === 0) {
+    return "";
+  } else {
+    // Return the first character of the trimmed string
+    return trimmedText[0];
+  }
 }
 
 // Do not change the code below
